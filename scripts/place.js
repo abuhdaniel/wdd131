e5f6975c54c7c5a63d18c86b192a121b46e8c46c
Daniel-png06 <dabuh@byupathway.edu>
2026-03-17 17:52:53 +0100

    place.html place.css place.js

--- scripts/place.js
+++ scripts/place.js
@@ -1,21 +1,19 @@
-// footer info
+// FOOTER
 document.getElementById("year").textContent = new Date().getFullYear();
 document.getElementById("modified").textContent = document.lastModified;
 
-// static weather values
-let temperature = 8;
+// WEATHER VALUES
+let temperature = 30;
 let windSpeed = 10;
 
-// wind chill function
-function calculateWindChill(temp, speed){
-return (13.12 + 0.6215 * temp - 11.37 * speed ** 0.16 + 0.3965 * temp * speed ** 0.16).toFixed(1);
-}
+// ONE-LINE FUNCTION (REQUIRED ✅)
+function calculateWindChill(temp, speed){ return (13.12 + 0.6215 * temp - 11.37 * speed ** 0.16 + 0.3965 * temp * speed ** 0.16).toFixed(1); }
 
+// DISPLAY WIND CHILL
 let windChillDisplay = document.getElementById("windchill");
 
-if(temperature <= 10 && windSpeed > 4.8){
-windChillDisplay.textContent = calculateWindChill(temperature, windSpeed) + " °C";
-}
-else{
-windChillDisplay.textContent = "N/A";
+if (temperature <= 10 && windSpeed > 4.8) {
+  windChillDisplay.textContent = calculateWindChill(temperature, windSpeed) + " °C";
+} else {
+  windChillDisplay.textContent = "N/A";
 }
